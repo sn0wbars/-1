de8d7476d11eea5ab66a499f6f66c7bf0f257a7e
sn0wbars <alex-borz@yandex.ru>
2015-11-21 12:41:50 +0300

    qq

---
+++ Проект1/list.cpp
@@ -0,0 +1,159 @@
+#pragma once
+
+#define CRT_SECURE_NO_WARNINGS
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <errno.h>
+#include <assert.h>
+#include "platypus.h"
+
+const unsigned int MAXnumber = 10000;
+
+//#define TYPEofLIST char
+template <typename T>
+class node
+{
+private:
+	node* m_prev;
+	node* m_next;
+	T m_val;
+	friend int node_OK(node* N) { return !(N->m_prev) && !(N->m_next)};
+	friend bool node_DUMP(const node* <T> N, const char name[], int error = 0);
+public:
+	node(const T value, node* prev = nullptr, node* next = nullptr) :
+		m_val(value), m_prev(prev), m_next(next) {};
+	~node() : m_val(-1), m_prev(nullptr), m_next(nullptr) {};
+	node* getnext() { return m_next }; // accessor
+	node* getprev() { return m_prev };
+	T getval() { return m_val };
+	bool putnext(node* next) { m_next = next };
+	bool putprev(node* prev) { m_prev = prev };
+	bool putval(T val) { m_val = val };
+};
+
+template<typename T>
+bool node_DUMP(const node* <T> N, const char name[], int error = 0) //todo: expanse
+{
+	printf("\nm_prev = %d\n m_next = %d\n m_val = %d\n", N->m_prev, N->m_next, N->m_val);
+	return 0;
+};
+
+template <typename type> 
+class List
+{
+private:
+	node<type> * first, * end;
+	int number;
+	bool find(node<type>* result, type val);
+	friend int List_OK(list* N) { return !(N->first && N->end && (number >= 0)) };
+	friend bool List_DUMP(const list* <T> N, const char name[], int error = 0);
+public:
+	List(type val = 0) : number(1)
+	{
+		first = new node<type>(n);
+		end = first;
+	}
+	bool push(type val); 
+	type pop() 
+	{
+		ASSERT_OK(this);
+		type result = end->getval();
+		node<type>* previous = end->getprev;
+		delete end;
+		end = previous;
+		end->putnext(nullptr);
+		ASSERT_OK(this);
+		return result;
+	};
+	bool insert();
+	bool remove();
+	~List();
+};
+
+template<typename type>
+bool List<type>::push(type val)
+{
+	ASSERT_OK(this);
+	end->putnext(new node<type>(val, end)); // 
+	assert(end->getnext());
+	end = end->getnext();
+	return 1;
+}
+
+template<typename type>
+type List<type>::pop()
+{
+	ASSERT_OK(this);
+	type result = end->getval();
+	node<type>* previous = end->getprev;
+	delete end;
+	end = previous;
+	end->putnext(nullptr);
+	ASSERT_OK(this);
+	return result;
+}
+
+template<typename T>
+bool List_DUMP(const list* <T> N, const char name[], int error = 0) //todo: expanse
+{ //!!!!!!
+	printf("\nm_prev = %d\n m_next = %d\n m_val = %d\n", N->m_prev, N->m_next, N->m_val);
+	return 0;
+};
+template<typename T>
+int List_OK(list* N)
+{ 
+	if (N->first && N->end && (number >= 0) && err) return 0;
+	else return 1;
+	node* ptr = N->first;
+	while (ptr->next)
+	{
+		ptr = ptr->next;
+			if !(node_OK(ptr))
+				return 1;
+	}
+	return 0;
+}
+
+//int main()
+//{
+//	return 0;
+//}
+
+//template<typename T>
+//inline node<T>::node(const T value, node * prev, node * next)
+//{
+//
+//}
+
+template<typename type>
+bool List<type>::find(node<type>* result, type val) // start
+{
+	ASSERT_OK(this);
+	node<type>* ptr = first;
+	for (int i = 0; i < lenght; ++i)
+	{
+		assert (ptr);
+		if (ptr->m_val == val)
+		{
+			result = ptr;
+			return 1;
+		}
+		ptr = ptr->m_next;
+	}
+	return 0;
+}
+
+template<typename type>
+inline List<type>::~List()
+{
+	ASSERT_OK(this);
+	node<type>* next = first;
+	for (int i = 0; i < number; i++)
+	{
+		node<type>* step = next;
+		next = step->m_next;
+		delete step;
+		assert(next && (i != number - 1));
+	}
+} 
---
+++ Проект1/stack.h
@@ -0,0 +1,165 @@
+#ifndef STACK_H_INCLUDED 
+#define STACK_H_INCLUDED
+
+#define _CRT_SECURE_NO_WARNINGS
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <errno.h>
+#include <assert.h>
+#include "platypus.h"
+
+#define TYPEofSTACK char
+
+const unsigned int MAXmemory = 33554432;
+
+struct Stack
+{
+	void* arr; // 
+	void* arrPtr;
+	size_t amount; // number of elements
+	size_t size; // number of allocated bytes for stack 
+	bool state = false;
+};
+
+inline TYPEofSTACK* ACCESS(void* a)
+{
+	return (TYPEofSTACK*)a;
+}
+
+int Stack_OK(Stack* This)
+{
+	if (This == nullptr) return This->state = 0, stack_is_nullptr;
+	if (!This->state) return stack_is_broken;
+	TYPEofSTACK* arr = ACCESS(This->arr);
+	TYPEofSTACK* arrPtr = ACCESS(This->arrPtr);
+	if (arr == nullptr) return This->state = 0, arr_is_nullptr;
+	//if (arrPtr == nullptr) return This->state = 0, arrPtr_is_nullptr;
+	if (!(arrPtr == nullptr))
+	{
+		if (arrPtr - arr < 0) return This->state = 0, arrPtr_is_less_arr;
+		if (This->amount < (int)(arrPtr - arr)) return This->state = 0, wrong_amount;
+	}
+	return This->state = 1, no_error; // υμ
+}
+bool Stack_DUMP(const Stack* This, const char name[], int error = 0)
+{
+	assert(This);
+	FILE* dumpFile = fopen("Stack_dump.txt", "a"); \
+		if (dumpFile == nullptr) return printf("Can't create a dump file"), 0;
+	fprintf(dumpFile, __TIME__ " " __DATE__"\n");
+	if (error) fprintf(dumpFile, "OMG! ERROR! ERROR!!11\n\nStack %s failed in %s/%s, %d\n",
+		name, __FILE__, __func__, __LINE__);
+	fprintf(dumpFile, "********************************************\n");
+	fprintf(dumpFile, " arr = %d\n arrPtr = %d\n size = %d\n amount = %d\n state = %d\n",
+		ACCESS(This->arr), ACCESS(This->arrPtr), This->size, This->amount, This->state);
+	if (!error || error == wrong_amount)
+		for (int i = 0; i*sizeof(TYPEofSTACK) < This->size; ++i)
+		{
+			fprintf(dumpFile, "arr[%d]: %d", i, ACCESS(This->arr)[i]);
+			if (i < (int)This->amount) fprintf(dumpFile, " *\n");
+			else fprintf(dumpFile, "\n");
+		}
+	fprintf(dumpFile, "********************************************\n\n\n");
+	//fprintf(dumpFile, "Ps: _HEAP_MAXREQ = %d", _HEAP_MAXREQ);
+	fclose(dumpFile);
+	return 1;
+}
+
+bool Stack_ctor(Stack* This, size_t buffer = 32)
+{
+	assert(This);
+	bool st = 1;
+
+	This->amount = 0;
+	if (buffer > MAXmemory)
+	{
+		st = 0;
+		buffer = MAXmemory;
+	}
+	This->size = buffer;
+	This->arr = calloc(buffer, sizeof(TYPEofSTACK));
+	This->arrPtr = nullptr;
+	This->state = true;
+
+	ASSERT_OK(Stack, This);
+	return st;
+}
+bool Stack_dctor(Stack* This)
+{
+	ASSERT_OK(Stack, This)
+		free(This->arr);
+	This->arr = NULL;
+	This->arrPtr = NULL;
+	This->amount = -1;
+	This->size = -1;
+	This->state = 0;
+
+	return 1;
+}
+
+bool Stack_push(Stack* This, TYPEofSTACK val)
+{
+	ASSERT_OK(Stack, This);
+
+	if ((This->amount + 1) * sizeof(TYPEofSTACK) > This->size)
+	{
+		if (This->size < MAXmemory)
+		{
+			This->size *= 2;
+			This->arr = realloc(ACCESS(This->arr), This->size);
+			This->arrPtr = ACCESS(This->arr) + This->amount;
+			ASSERT_OK(Stack, This);
+		}
+		else return 0;
+	}
+
+	else if (This->arrPtr == nullptr) This->arrPtr = This->arr;
+	else This->arrPtr = ACCESS(This->arrPtr) + 1;
+
+	*ACCESS(This->arrPtr) = val;
+	++This->amount;
+
+	ASSERT_OK(Stack, This);
+	return 1;
+
+}
+bool Stack_pop(Stack* This, TYPEofSTACK* val)
+{
+	ASSERT_OK(Stack, This);
+	assert(val);
+
+	if (This->arrPtr == nullptr)
+		return 0;
+
+	*val = *ACCESS(This->arrPtr);
+	--This->amount;
+	if (This->arrPtr == This->arr)
+		This->arrPtr = nullptr;
+	else This->arrPtr = ACCESS(This->arrPtr) - 1;
+
+	ASSERT_OK(Stack, This);
+	return 1;
+}
+
+//
+//int main() // stack test
+//{
+// Stack dima;
+// assert(Stack_ctor(&dima, 5));
+// assert(Stack_push(&dima, 102));
+// Stack_push(&dima, 102);
+// Stack_push(&dima, 102);
+// char a;
+// assert(Stack_pop(&dima, &a));
+// (Stack_pop(&dima, &a));
+// (Stack_pop(&dima, &a));
+// (Stack_pop(&dima, &a));
+// (Stack_pop(&dima, &a));
+// Stack_DUMP(&dima, "dima");
+// printf("%d", a);
+// Stack_dctor(&dima);
+// return 0;
+//}
+
+#endif
